bb9636a4f34ad0ef45571d771451955f20e2e2bf
sarah-intake40 <sarahrefaatss@gmail.com>
2020-06-06 00:33:42 +0200

    edit migration

--- Together/database/migrations/2020_05_17_191255_create_groups_table.php
+++ Together/database/migrations/2020_05_17_191255_create_groups_table.php
@@ -14,6 +14,7 @@ class CreateGroupsTable extends Migration
     public function up()
     {
         Schema::create('groups', function (Blueprint $table) {
+            
             $table->id();
             $table->string('photo')->nullable();
             $table->string('address')->nullable();
